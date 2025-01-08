8db9c5c7e13c0ec4367ce5780b237631eb2fdf3c
codepzj <email@codepzj.cn>
2025-01-08 19:14:16 +0800

    feat: 更新配置

--- .vitepress/blog-theme.js
+++ .vitepress/blog-theme.js
@@ -1,29 +1,11 @@
 import { getThemeConfig } from "@sugarat/theme/node";
 
-const baseUrl = "https://docs.codepzj.cn";
-const RSS = {
-  title: "浩瀚星河的文档",
-  baseUrl,
-  copyright: "Copyright (c) 2024-2025, 浩瀚星河",
-};
 const blogTheme = getThemeConfig({
-  RSS,
   author: "浩瀚星河",
   mermaid: true,
   search: false,
   themeColor: "vp-green",
   recommend: false,
-  comment: {
-    type: "giscus",
-    options: {
-      repo: "codepzj/docs-comment",
-      repoId: "R_kgDONmy5_Q",
-      category: "Show and tell",
-      categoryId: "DIC_kwDONmy5_c4ClzWq",
-      inputPosition: "top",
-    },
-    mobileMinify: true,
-  },
 });
 
 export { blogTheme };
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,5 +1,3 @@
 import BlogTheme from "@sugarat/theme";
 
-import "./style.css"
-
 export default BlogTheme;
